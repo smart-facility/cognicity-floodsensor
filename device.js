4fbaaca6346cddee3ce3a68b63e6f9db1ac610c6
Matthew Berryman <mberryma@uow.edu.au>
2016-03-09 15:55:51 +1100

    First cut at including sensor

--- device.js
+++ device.js
@@ -1,5 +1,7 @@
 var awsIot = require('aws-iot-device-sdk');
 
+var usonic = require('r-pi-usonic');
+
 var device = awsIot.device({
    keyPath: './awsCerts/private.pem.key',
   certPath: './awsCerts/certificate.pem.crt',
@@ -12,14 +14,21 @@ var device = awsIot.device({
 // Device is an instance returned by mqtt.Client(), see mqtt.js for full
 // documentation.
 //
+
+
+
+
 device
   .on('connect', function() {
     console.log('connect');
-    device.subscribe('topic/floodsensor');
-    device.publish('topic/floodsensor', JSON.stringify({ test_data: 1}));
+    usonic.init(function (error) {
+        if (error) {
+            console.log('Error')
+        } else {
+          var sensor = usonic.createSensor(24, 23, 450);
+          var sensor_reading = sensor();
+          device.publish('topic/floodsensor', JSON.stringify({ test_data: sensor_reading}));
+          console.log('published'+JSON.stringify({ test_data: sensor_reading}));
+        }
     });
-
-device
-  .on('message', function(topic, payload) {
-    console.log('message', topic, payload.toString());
   });

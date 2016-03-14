215ecae5819c34ef55f38fbab7316b2a2b7d2116
Matthew Berryman <mberryma@uow.edu.au>
2016-03-14 16:19:14 +1100

    Add more variables to config, tidy up code.

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 config = {};
-config.max_depth = 17; // cm
+config.max_distance = 17.1; // distance in cm of the flood sensor above lowest possible water level. 
+config.aws = {};
+config.aws.region = 'us-west-2'; // AWS region your IoT service is set up in
+config.aws.clientId = process.env.CLIENT_ID; // Optional, AWS device client ID
 module.exports = config;
--- device.js
+++ device.js
@@ -1,37 +1,28 @@
+var config = require('./config.js');
 var awsIot = require('aws-iot-device-sdk');
-
 var usonic = require('r-pi-usonic');
 
 var device = awsIot.device({
    keyPath: './awsCerts/private.pem.key',
   certPath: './awsCerts/certificate.pem.crt',
     caPath: './rootCA.pem',
-  clientId: process.env.CLIENT_ID,
-    region: 'us-west-2'
+  clientId: config.aws.clientId,
+    region: config.aws.region
 });
 
-
-
-//
-// Device is an instance returned by mqtt.Client(), see mqtt.js for full
-// documentation.
-//
-
-var config = require('./config.js');
-
-// Need to put the following in a polling sensor loop
 device
   .on('connect', function() {
     console.log('connect');
-    usonic.init(function (error) {
+    usonic.init(function (error) { // initialise the sensor
         if (error) {
             console.log('Error')
         } else {
           setInterval( function() {
             var sensor = usonic.createSensor(24, 23, 450);
             var sensor_reading = sensor().toFixed(2);
-            device.publish('topic/floodsensor', JSON.stringify({coordinates: [150.87843, -34.405404], height: config.max_depth - sensor_reading}));
-            console.log('published'+JSON.stringify({coordinates: [150.87843, -34.405404], height: config.max_depth - sensor_reading}));
+            // publish heigh = max_distance - distance from sensor_reading.
+            device.publish('topic/floodsensor', JSON.stringify({coordinates: [150.87843, -34.405404], height: config.max_distance - sensor_reading}));
+            console.log('published'+JSON.stringify({coordinates: [150.87843, -34.405404], height: config.max_distance - sensor_reading}));
           },3000);
         }
     });

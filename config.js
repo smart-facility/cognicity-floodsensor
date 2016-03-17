c97c7a92a0efd0091c3ddfb1f41a4ce2d8ca1746
Matthew Berryman <mattjb@me.com>
2016-03-17 12:48:32 +1100

    make sense publishing interval a config variable

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 config = {};
 config.max_distance = 17.1; // distance in cm of the flood sensor above lowest possible water level. 
+config.interval = 3600; // seconds between sensor values being uploaded
 config.aws = {};
 config.aws.region = 'us-west-2'; // AWS region your IoT service is set up in
 config.aws.clientId = process.env.CLIENT_ID; // Optional, AWS device client ID

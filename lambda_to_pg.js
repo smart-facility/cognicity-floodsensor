437399ed4476d8fa020e307d9f1bf4cf7830fccd
Tomas <tomas@uow.edu.au>
2016-06-16 12:26:04 +1000

    prototyping database measurement insert

--- lambda_to_pg.js
+++ lambda_to_pg.js
@@ -1,9 +1,11 @@
-var pg = require('pg')
-var config = require
+var pg = require('pg');
+var config = require('./config.js');
 
 exports.handler = function(event, context) {
   console.log('Received event:');
   console.log(JSON.stringify(event, null, '  '));
+
+  var obs = JSON.stringify(event);
 
   // do stuff to store in pg
   var conn = "pg://user:password@host:5432/bd_name"; // Could set via config stored in S3 or in Lambda
@@ -12,6 +14,23 @@ exports.handler = function(event, context) {
   client.connect();
 
   var query = client.query("SELECT * FROM BLA WHERE ID = 1"); // do something with event, which is object as per JSON emitted in device.js#24
+
+  var query = client.query(
+    {
+      text: "INSERT INTO sensor_data (sensor_id, measurement_time, distance)" +
+      "VALUES (" +
+      "$1, " +
+      "to_timestamp($2), " +
+      "$3" +
+      ");",
+      values: [
+        config.clientId,
+        obs.time,
+        obs.distance
+      ]
+    }
+  );
+
   query.on("row", function (row, result) {
     result.addRow(row);
   });

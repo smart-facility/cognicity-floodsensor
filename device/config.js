55191b8c47f1d4b38bc8e781bc595f587f72df53
Matthew Berryman <mattjb@me.com>
2016-07-14 10:38:01 +1000

    Log to files using winston

--- device/config.js
+++ device/config.js
@@ -2,6 +2,16 @@ config = {};
 config.interval = 1800; // seconds between sensor values being uploaded
 config.clientId = 1; // client ID, integer, must match sensor_metadata table
 config.hasDHT = true; // if there's a DHT temperature and humidity sensor attached
+
+
 config.aws = {};
 config.aws.region = 'us-west-2'; // AWS region your IoT service is set up in
+
+// Logging configuration
+config.logger = {};
+config.logger.level = "info"; // What level to log at; info, verbose or debug are most useful. Levels are (npm defaults): silly, debug, verbose, info, warn, error.
+config.logger.maxFileSize = 1024 * 1024 * 100; // Max file size in bytes of each log file; default 100MB
+config.logger.maxFiles = 10; // Max number of log files kept
+config.logger.logDirectory = '.'; // Set this to a full path to a directory - if not set logs will be written to the application directory.
+
 module.exports = config;
--- device/device.js
+++ device/device.js
@@ -2,6 +2,7 @@ var config = require('./config.js');
 var awsIot = require('aws-iot-device-sdk');
 var usonic = require('r-pi-usonic');
 var dht_sensor_lib = require('node-dht-sensor');
+var logger = require('winston');
 
 var device = awsIot.device({
    keyPath: './awsCerts/private.pem.key',
@@ -11,12 +12,36 @@ var device = awsIot.device({
     region: config.aws.region
 });
 
+// Set up logging to log files
+var logPath = ( config.logger.logDirectory ? config.logger.logDirectory : __dirname );
+// Check that log file directory can be written to
+try {
+	fs.accessSync(logPath, fs.W_OK);
+} catch (e) {
+	console.log( "Log directory '" + logPath + "' cannot be written to"  );
+	throw e;
+}
+logPath += path.sep;
+logPath += config.instance + ".log";
+
+logger
+	.add(logger.transports.File, {
+		filename: logPath, // Write to projectname.log
+		json: false, // Write in plain text, not JSON
+		maxsize: config.logger.maxFileSize, // Max size of each file
+		maxFiles: config.logger.maxFiles, // Max number of files
+		level: config.logger.level // Level of log messages
+	})
+	// Console transport is no use to us when running as a daemon
+	.remove(logger.transports.Console);
+
+
 device
   .on('connect', function() {
-    console.log('connect');
+    logger.info('connect');
     usonic.init(function (error) { // initialise the sensor
         if (error) {
-            console.log('Error')
+            logger.error('Error initialising ultrasound sensor.');
         } else {
           var ultrasound_sensor = usonic.createSensor(24, 23, 450);
           if (config.hasDHT) {
@@ -35,9 +60,10 @@ device
                 if (config.hasDHT) {
                   var dht_readout = dht_sensor_lib.read();
                   device.publish('topic/floodsensor', JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading, temperature: dht_readout.temperature.toFixed(2), humidity: dht_readout.humidity.toFixed(2)}));
+                  logger.info('published '+JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading, temperature: dht_readout.temperature.toFixed(2), humidity: dht_readout.humidity.toFixed(2)}));
                 } else {
                   device.publish('topic/floodsensor', JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading}));
-                  console.log('published'+JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading}));
+                  logger.info('published '+JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading}));
                 }
                 clearInterval(averagingInterval);
               }

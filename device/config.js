54faa59434e97e000965a77a17b700a36b19f346
Matthew Berryman <mattjb@me.com>
2016-07-15 12:06:32 +1000

    Change default log path

--- device/config.js
+++ device/config.js
@@ -12,6 +12,6 @@ config.logger = {};
 config.logger.level = "info"; // What level to log at; info, verbose or debug are most useful. Levels are (npm defaults): silly, debug, verbose, info, warn, error.
 config.logger.maxFileSize = 1024 * 1024 * 100; // Max file size in bytes of each log file; default 100MB
 config.logger.maxFiles = 10; // Max number of log files kept
-config.logger.logDirectory = '.'; // Set this to a full path to a directory - if not set logs will be written to the application directory.
+config.logger.logDirectory = '/home/pi/cognicity-floodsensor/logs'; // Set this to a full path to a directory - if not set logs will be written to the application directory.
 
 module.exports = config;

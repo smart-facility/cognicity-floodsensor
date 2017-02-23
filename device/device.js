081b39e0f3ac438495ce7bc1d36fd544d0778369
Matthew Berryman <mattjb@me.com>
2017-02-23 12:49:52 +1100

    kill reference to AWS device object

--- device/device.js
+++ device/device.js
@@ -18,99 +18,97 @@ try {
 logPath += path.sep + config.logger.logFileName;
 
 logger
-	.add(logger.transports.File, {
-		filename: logPath, // Write to projectname.log
-		json: false, // Write in plain text, not JSON
-		maxsize: config.logger.maxFileSize, // Max size of each file
-		maxFiles: config.logger.maxFiles, // Max number of files
-		level: config.logger.level // Level of log messages
-	})
-	// Console transport is no use to us when running as a daemon
-	.remove(logger.transports.Console);
+.add(logger.transports.File, {
+	filename: logPath, // Write to projectname.log
+	json: false, // Write in plain text, not JSON
+	maxsize: config.logger.maxFileSize, // Max size of each file
+	maxFiles: config.logger.maxFiles, // Max number of files
+	level: config.logger.level // Level of log messages
+})
+// Console transport is no use to us when running as a daemon
+.remove(logger.transports.Console);
 
 
 
 var publish = function(message) {
 
 
-  var conn = config.pg.conString; // Could set via config stored in S3 or in Lambda
+	var conn = config.pg.conString; // Could set via config stored in S3 or in Lambda
 
-  if (Number(event.distance) < 450 && Number(event.temperature) > 0) {
+	if (Number(event.distance) < 450 && Number(event.temperature) > 0) {
 
-    var client = new pg.Client(conn);
-    client.connect();
+		var client = new pg.Client(conn);
+		client.connect();
 
-    var query = client.query(
-      {
-        text: "INSERT INTO sensor_data (sensor_id, measurement_time, distance, temperature, humidity)" +
-        "VALUES (" +
-        "$1, " +
-        "to_timestamp($2), " +
-        "$3," +
-        "$4," +
-        "$5" +
-        ");",
-        values: [
-          event.id,
-          event.time/1000,
-          event.distance,
-          event.temperature,
-          event.humidity
-        ]
-      }
-    );
+		var query = client.query(
+			{
+				text: "INSERT INTO sensor_data (sensor_id, measurement_time, distance, temperature, humidity)" +
+				"VALUES (" +
+				"$1, " +
+				"to_timestamp($2), " +
+				"$3," +
+				"$4," +
+				"$5" +
+				");",
+				values: [
+					event.id,
+					event.time/1000,
+					event.distance,
+					event.temperature,
+					event.humidity
+				]
+			}
+		);
 
-    query.on("row", function (row, result) {
-      result.addRow(row);
-    });
+		query.on("row", function (row, result) {
+			result.addRow(row);
+		});
 
-    query.on("end", function (result) {
-      var jsonString = JSON.stringify(result.rows);
-      var jsonObj = JSON.parse(jsonString);
-      client.end();
-      context.succeed(jsonObj);
-      context.done(null,'finished successfully');
-    });
-  } else {
-      var jsonString = JSON.stringify(event);
-      var jsonObj = JSON.parse(jsonString);
-      logger.info('finished successfully (filtered)');
-  }
+		query.on("end", function (result) {
+			var jsonString = JSON.stringify(result.rows);
+			var jsonObj = JSON.parse(jsonString);
+			client.end();
+			context.succeed(jsonObj);
+			context.done(null,'finished successfully');
+		});
+	} else {
+		var jsonString = JSON.stringify(event);
+		var jsonObj = JSON.parse(jsonString);
+		logger.info('finished successfully (filtered)');
+	}
 }
 
-device
-  .on('connect', function() {
-    logger.info('connect');
-    usonic.init(function (error) { // initialise the sensor
-        if (error) {
-            logger.error('Error initialising ultrasound sensor.');
-        } else {
-          var ultrasound_sensor = usonic.createSensor(24, 23, config.usonic_timeout);
-          if (config.hasDHT) {
-            dht_sensor_lib.initialize(22,4);
-          }
-          setInterval( function() {
-            var average = 0;
-            var count = 0;
-            var averagingInterval = setInterval( function() {
-              if (count < 5) {
-                count++;
-                average += ultrasound_sensor();
-              } else {
-                var ultrasound_sensor_reading = (average/5).toFixed(2);
-                // publish reading
-                if (config.hasDHT) {
-                  var dht_readout = dht_sensor_lib.read();
-                  publish(JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading, temperature: dht_readout.temperature.toFixed(2), humidity: dht_readout.humidity.toFixed(2)}));
-                  logger.info('published '+JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading, temperature: dht_readout.temperature.toFixed(2), humidity: dht_readout.humidity.toFixed(2)}));
-                } else {
-                  publish(JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading}));
-                  logger.info('published '+JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading}));
-                }
-                clearInterval(averagingInterval);
-              }
-            }, 1000);
-          }, config.interval*1000);
-        }
-      });
-  });
+
+logger.info('connect');
+usonic.init(function (error) { // initialise the sensor
+	if (error) {
+		logger.error('Error initialising ultrasound sensor.');
+	} else {
+		var ultrasound_sensor = usonic.createSensor(24, 23, config.usonic_timeout);
+		if (config.hasDHT) {
+			dht_sensor_lib.initialize(22,4);
+		}
+		setInterval( function() {
+			var average = 0;
+			var count = 0;
+			var averagingInterval = setInterval( function() {
+				if (count < 5) {
+					count++;
+					average += ultrasound_sensor();
+				} else {
+					var ultrasound_sensor_reading = (average/5).toFixed(2);
+					// publish reading
+					if (config.hasDHT) {
+						var dht_readout = dht_sensor_lib.read();
+						publish(JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading, temperature: dht_readout.temperature.toFixed(2), humidity: dht_readout.humidity.toFixed(2)}));
+						logger.info('published '+JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading, temperature: dht_readout.temperature.toFixed(2), humidity: dht_readout.humidity.toFixed(2)}));
+					} else {
+						publish(JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading}));
+						logger.info('published '+JSON.stringify({id: config.clientId, time: (new Date()).valueOf(), distance: ultrasound_sensor_reading}));
+					}
+					clearInterval(averagingInterval);
+				}
+			}, 1000);
+		}, config.interval*1000);
+	}
+});
